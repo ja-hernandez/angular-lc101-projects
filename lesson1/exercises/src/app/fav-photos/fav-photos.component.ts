6f5348e2d3c0f704ab112ec494b70dfab9e2940b
Jose Hernandez <jose.a.hernandez.g@gmail.com>
2020-12-07 15:04:09 -0600

    part 1 of exercises

--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -6,10 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fav-photos.component.css']
 })
 export class FavPhotosComponent implements OnInit {
-  photosTitle = 'Photo/Image Section Title Here';
+  photosTitle = 'Some of my favorite photos!';
   image1 = 'https://www.launchcode.org/assets/icons/target-00c7a0bc7776901e3c5cdc4c36c465f19e41a249bc7267a595f0bfa14ab944b7.png';
-  image2 = '';
-  image3 = '';
+  image2 = '..\/..\/assets/dart-clipart-tranquilizer-dart-19-transparent.png';
+  image3 = 'https://cdn.vox-cdn.com/thumbor/p-MQqakFytgblP7PidTHmen1FF8=/0x0:640x400/920x613/filters:focal(269x149:371x251):format(webp)/cdn.vox-cdn.com/uploads/chorus_image/image/57993335/aolrunningman.0.png';
 
   constructor() { }
 
